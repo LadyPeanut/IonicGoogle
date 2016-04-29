aa2c3a6cea26e4edc72c4cd84d85a44c546c24d7
Patricia Lazaro <plazaro1994@gmail.com>
2016-04-29 18:57:40 +0200

    primera version funcionando

--- google/www/js/app.js
+++ google/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,101 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('welcome', {
+    url: '/welcome',
+    templateUrl: "views/welcome.html",
+    controller: 'WelcomeCtrl'
+  })
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "views/sidemenu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "views/home.html",
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/welcome');
+})
+
+.controller('WelcomeCtrl', function($scope, $state, UserService, $ionicLoading) {
+  // This method is executed when the user press the "Sign in with Google" button
+  $scope.googleSignIn = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
+    });
+
+    window.plugins.googleplus.login(
+      {},
+      function (user_data) {
+        // For the purpose of this example I will store user data on local storage
+        UserService.setUser({
+          userID: user_data.userId,
+          name: user_data.displayName,
+          email: user_data.email,
+          picture: user_data.imageUrl,
+          accessToken: user_data.accessToken,
+          idToken: user_data.idToken
+        });
+
+        $ionicLoading.hide();
+        $state.go('app.home');
+      },
+      function (msg) {
+        $ionicLoading.hide();
+      }
+    );
+  };
+})
+
+.controller('AppCtrl', function($scope){
+
+})
+
+.controller('HomeCtrl', function($scope, UserService, $ionicActionSheet, $state, $ionicLoading){
+	$scope.user = UserService.getUser();
+
+	$scope.showLogOutMenu = function() {
+		var hideSheet = $ionicActionSheet.show({
+			destructiveText: 'Logout',
+			titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
+			cancelText: 'Cancel',
+			cancel: function() {},
+			buttonClicked: function(index) {
+				return true;
+			},
+			destructiveButtonClicked: function(){
+				$ionicLoading.show({
+					template: 'Logging out...'
+				});
+				// Google logout
+				window.plugins.googleplus.logout(
+					function (msg) {
+						console.log(msg);
+						$ionicLoading.hide();
+						$state.go('welcome');
+					},
+					function(fail){
+						console.log(fail);
+					}
+				);
+			}
+		});
+	};
+});
